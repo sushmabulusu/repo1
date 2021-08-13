ece49fc8c4416960f798ddfd746c22462877843f
sushmabulusu <sushma12bulusu@gmail.com>
2021-08-13 19:02:32 +0530

    repo1

---
+++ Cla.java
@@ -0,0 +1,23 @@
+
+public class Cla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubclass ABCD
+		{
+		      //PROGRAM FINDS WHETHER THERE IS A COMMAND LINE AGRGUMENT OR NOT AND SEPERATES BY COMMA
+		       if (args.length ==0)
+		            {
+		            System.out.println("No Values");
+		    }
+		        else{
+		            for(String i : args)     //for each value in args store in i
+		                System.out.print(i+" , ");
+		       
+		        }
+		        }
+		        }
+
+
+	}
+
+
